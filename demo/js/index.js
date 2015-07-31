3cde80a2bc03d716cdb27e50f17b7cd838941115
kong <k849996781@vip.qq.com>
2015-07-31 18:08:37 +0800

    初始版本，功能正常，有待提升

---
+++ demo/js/index.js
@@ -0,0 +1,21 @@
+function pw(){
+	
+	
+	
+	/*代码写在这里面，不用写window.onload*/
+	
+	var oDiv1 = document.getElementById('div1');
+	var oDiv2 = document.getElementById('div2');
+	var oNew = document.getElementsByClassName('newspaper')[0];
+	
+	
+	oDiv1.style.animation = 'my 5s 5';
+	
+	oDiv2.style.boxAlign = 'center';
+	oDiv2.style.boxPack = 'center';
+	
+	
+	oNew.style.columnCount = 3;
+	
+
+}
